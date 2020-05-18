bf1393350351b5d6b6b1de4d8720fbcaa839363a
Mike Wallick <mike.wallick@okta.com>
2020-05-17 19:07:58 -0500

    removed local okta admin sdk

--- okta/__init__.py
+++
@@ -1,7 +0,0 @@
-#from .admin import OktaAdmin
-from .auth import OktaAuth
-from .factors import OktaFactors
-from .groups import OktaGroups
-from .rest import RestUtil
-from .users import OktaUsers
-from .util import OktaUtil
