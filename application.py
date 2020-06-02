736656958b2b5c5da23941d22002af3e88390f17
Mike Wallick <mike.wallick@okta.com>
2020-06-02 11:25:30 -0500

    changes default port to 8000

--- application.py
+++ application.py
@@ -5,4 +5,4 @@ app = create_app()
 
 if __name__ == "__main__":
     # This is to run on c9.io or locally; you may need to change or make your own runner
-    app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 3000)), debug=True)
+    app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8000)), debug=True)
