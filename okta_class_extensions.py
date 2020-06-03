a88b7af922ef6cd97bbac49d8bb8c3bc627169a8
Mike Wallick <mike.wallick@okta.com>
2020-06-03 12:11:54 -0500

    updates README and User class extension documentation

--- okta_class_extensions.py
+++ okta_class_extensions.py
@@ -1,19 +1,6 @@
 from okta.models.user.User import User
 from okta.models.user.UserProfile import UserProfile
 
-"""
-If you have custom fields in your Okta user profile
-you simply need to extend the User and UserProfile
-classes. From there, you need to tell the UsersClient
-that you're using a custom User class.
-
-usersClient = UsersClient(
-    base_url="https://<YOUR_ORG>.okta.com", 
-    api_token="YOUR API TOKEN",
-    user_class=ExtendedUser
-)
-"""
-
 
 class ExtendedUser(User):
     def __init__(self, **kwargs):
@@ -24,7 +11,6 @@ class ExtendedUser(User):
 
 class ExtendedUserProfile(UserProfile):
     types = {
-        'goals': str,
         'windows_username': str,
         'sfdc_id': str
     }
@@ -33,6 +19,5 @@ class ExtendedUserProfile(UserProfile):
         # merge the types dict from super
         self.types.update(super().types)
 
-        self.goals = None
         self.windows_username = None
         self.sfdc_id = None
