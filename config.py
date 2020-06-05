8ef387eab0802ea8359ab255eb1597ca16ee3d50
Mike Wallick <mike.wallick@okta.com>
2020-06-05 09:44:51 -0500

    makes minor code formatting changes

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-from os import environ, path
+import os
+from os import path
 from dotenv import load_dotenv
 
 basedir = path.abspath(path.dirname(__file__))
@@ -7,25 +8,24 @@ load_dotenv(path.join(basedir, ".env"))
 
 class Config:
     """ Base config """
-    SECRET_KEY = environ.get("SECRET_KEY")
-    ORG_NAME = environ.get("ORG_NAME")
-    API_TOKEN = environ.get("API_TOKEN")
-    ISSUER = environ.get("ISSUER")
-    AUDIENCE = environ.get("AUDIENCE")
-    CACHE_METHOD = environ.get("CACHE_METHOD", "file")
-    BUCKET_NAME = environ.get("S3_BUCKET", None)
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    ORG_NAME = os.getenv("ORG_NAME")
+    API_TOKEN = os.getenv("API_TOKEN")
+    ISSUER = os.getenv("ISSUER")
+    AUDIENCE = os.getenv("AUDIENCE")
+    CACHE_METHOD = os.getenv("CACHE_METHOD", "file")
+    BUCKET_NAME = os.getenv("S3_BUCKET", None)
 
 
 class ProdConfig(Config):
     FLASK_ENV = "production"
     DEBUG = False
     TESTING = False
-    LOG_LEVEL = environ.get("LOG_LEVEL", "WARNING")
+    LOG_LEVEL = os.getenv("LOG_LEVEL", "WARNING")
 
 
 class DevConfig(Config):
-    # flask settings
     FLASK_ENV = "development"
     DEBUG = True
     TESTING = True
-    LOG_LEVEL = environ.get("LOG_LEVEL", "DEBUG")
+    LOG_LEVEL = os.getenv("LOG_LEVEL", "DEBUG")
